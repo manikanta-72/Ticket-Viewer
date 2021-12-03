eed3634210bf79da40dc86f0c21ad12f850578a9
manikanta-72 <62173646+manikanta-72@users.noreply.github.com>
2021-12-02 19:07:39 -0800

    Initial commit
    As part of coding challenge

---
+++ viewerapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+from .credentials import ZENDESK_PASSWORD, ZENDESK_USERNAME, ZENDESK_SUBDOMAIN
+from .models import Ticket
+from django.core.paginator import Paginator
+
+from django.http import HttpResponse
+
+import requests
+
+# Create your views here.
+
+def index(request):
+    #tickets = Ticket.objects.order_by('-created_at')[:5]
+    tickets = requests.get("https://{0}.zendesk.com/api/v2/tickets/".format(ZENDESK_SUBDOMAIN), auth=(ZENDESK_USERNAME,ZENDESK_PASSWORD))
+
+    if tickets.status_code != requests.codes.ok:
+        return render(request,'error.html',{})
+    #print(type(tickets))
+    #return render(request,'index.html',{'tickets':tickets})
+    #return render(request,'index.html',tickets)    
+    tickets = tickets.json()
+    paginator = Paginator(tickets['tickets'],25)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request,'index.html',{'page_obj':page_obj})
+    
+
+def ticket_by_id(request, ticket_id):
+    #ticket = Ticket.objects.get(pk=ticket_id)
+    ticket = requests.get("https://{0}.zendesk.com/api/v2/tickets/{1}.json".format(ZENDESK_SUBDOMAIN,ticket_id), auth=(ZENDESK_USERNAME,ZENDESK_PASSWORD)).json()
+    #return render(request, 'ticket_by_id.html', {'ticket':ticket})
+    return render(request, 'ticket_by_id.html', ticket)
